66f1b5b981d695ab513265830451bb2eabb1dfa2
adolfoiracheta <84049381+adolfoiracheta@users.noreply.github.com>
2023-02-24 17:06:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Arrays-For-While-Loops-Colors-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-zfaww7)
---
+++ index.js
@@ -0,0 +1,75 @@
+
+
+//array with for loops
+//condition is <
+let myColors = ['blue', 'gold', 'red', 'black'];
+for(let i=0; i<4; i++) {
+  console.log(myColors[i])
+}
+
+/*
+//condition is <=
+let myFavoriteColors = ['blue', 'gold', 'red', 'black', 'white'];
+for(let i=0; i<=4; i++) {
+  console.log(myFavoriteColors[i])
+}
+*/
+
+/*
+//condition is < array.length
+let myFavoriteColors = ['blue', 'gold', 'red', 'black', 'white'];
+for(let i=0; i<myFavoriteColors.length; i++) {
+  console.log(myFavoriteColors[i])
+}
+*/
+
+/*
+//array with while loop
+let i=0
+while (i<myColors.length) {
+console.log(myColors[i])
+i++
+}
+*/
+
+/*
+//array with "Do While Loop"
+let i=0
+do {
+  console.log(myColors[i])
+  i++
+}
+while (i<myColors.length) {
+//delete code between these curly brackets is not needed (undefined)
+  console.log(myColors[i])
+}
+*/
+
+
+//array with "Do While Loop"
+//delete "let". It can only be used once in a variable.
+i=0
+do {
+console.log(myColors[i])
+i++
+}
+while (i<myColors.length)
+
+
+//array with "Do While Loop"
+//program to display numbers 1 to 5
+i=1;
+const n=5;
+do {
+  console.log(i);
+  i++
+}
+while(i<=n);
+
+
+
+
+
+
+
+
